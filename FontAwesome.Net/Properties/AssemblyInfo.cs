7d4ff4b757e782ccc072f3fba7878ad6d3a0b965
Chris Rodgers <christophano@hotmail.com>
2014-11-10 10:43:27 +0000

    Initial Commit

---
+++ FontAwesome.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Font Awesome Dot Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Font Awesome Dot Net")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("faace2a4-6981-43e6-bc71-35fbb31be17c")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
